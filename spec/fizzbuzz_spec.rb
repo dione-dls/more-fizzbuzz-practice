e40ad1d3df81113db9c0c6446fe7632c6f6f6c5c
Dione <dione.estabillo@gmail.com>
2018-04-09 18:19:44 +0000

    Adding more tests for numbers beyond 20 and numbers entered as formulas

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -24,3 +24,20 @@ describe 'fizzbuzz' do
     end
 end
 
+describe 'fizzbuzz' do
+    it 'returns "buzz" when passed 100' do
+        expect(fizzbuzz(100)).to eq 'buzz'
+    end
+end
+
+describe 'fizzbuzz' do
+    it 'returns "fizz" when passed (21/7)' do
+        expect(fizzbuzz(21/7)).to eq 'fizz'
+    end
+end
+
+describe 'fizzbuzz' do
+    it 'returns "fizzbuzz" when passed (10 * 3)' do
+        expect(fizzbuzz(10 * 3)).to eq 'fizzbuzz'
+    end
+end
